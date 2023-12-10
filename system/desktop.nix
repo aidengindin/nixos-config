28cd6cc90793c72f4b426b9dd4556db08dea07d3
Aiden Gindin <aiden@aidengindin.com>
2023-12-10 09:39:13 -0500

    re-enable sound

--- system/desktop.nix
+++ system/desktop.nix
@@ -10,6 +10,15 @@ in
 
   config = mkIf cfg.enable {
     services.xserver.desktopManager.gnome.enable = true;
+    sound.enable = true;
+    hardware.pulseaudio.enable = false;
+    security.rtkit.enable = true;
+    services.pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+    };
   };
 }
 
