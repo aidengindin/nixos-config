1f25b48724cded7b8da7cdb356a46cdc01cf7935
Aiden Gindin <aiden@thegindins.com>
2023-12-11 11:54:02 -0500

    enable zsh on linux

--- hosts/shadowfax/default.nix
+++ hosts/shadowfax/default.nix
@@ -7,5 +7,6 @@
   # Make sure the Nix daemon always runs
   services.nix-daemon.enable = true;
 
+  # Since this isn't Linux, we can't depend on this being set in system
   programs.zsh.enable = true;
 }
--- system/default.nix
+++ system/default.nix
@@ -7,51 +7,55 @@
     ./ssh.nix
   ];
 
-  config.nixpkgs.config.allowUnfree = true;
+  config = {
+    nixpkgs.config.allowUnfree = true;
 
-  config.users.users.agindin = {
-    isNormalUser = true;
-    description = "agindin";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [];
+    users.users.agindin = {
+      isNormalUser = true;
+      description = "agindin";
+      extraGroups = [ "networkmanager" "wheel" ];
+      packages = with pkgs; [];
+    };
+
+    time.timeZone = "America/New_York";
+
+    # Select internationalisation properties.
+    i18n.defaultLocale = "en_US.UTF-8";
+
+    i18n.extraLocaleSettings = {
+      LC_ADDRESS = "en_US.UTF-8";
+      LC_IDENTIFICATION = "en_US.UTF-8";
+      LC_MEASUREMENT = "en_US.UTF-8";
+      LC_MONETARY = "en_US.UTF-8";
+      LC_NAME = "en_US.UTF-8";
+      LC_NUMERIC = "en_US.UTF-8";
+      LC_PAPER = "en_US.UTF-8";
+      LC_TELEPHONE = "en_US.UTF-8";
+      LC_TIME = "en_US.UTF-8";
+    };
+
+    # Configure keymap in X11
+    services.xserver = {
+      layout = "us";
+      xkbVariant = "";
+    };
+
+    nix = {
+      package = pkgs.nixFlakes;
+      extraOptions = ''
+        experimental-features = nix-command flakes
+      '';
+    };
+
+    networking.networkmanager.enable = true;
+    services.tailscale.enable = true;
+
+    programs.zsh.enable = true;
+
+    environment.systemPackages = with pkgs; [
+      htop
+      neovim
+    ];
   };
-
-  config.time.timeZone = "America/New_York";
-
-  # Select internationalisation properties.
-  config.i18n.defaultLocale = "en_US.UTF-8";
-
-  config.i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
-  };
-
-  # Configure keymap in X11
-  config.services.xserver = {
-    layout = "us";
-    xkbVariant = "";
-  };
-
-  config.nix = {
-    package = pkgs.nixFlakes;
-    extraOptions = ''
-      experimental-features = nix-command flakes
-    '';
-  };
-
-  config.networking.networkmanager.enable = true;
-  config.services.tailscale.enable = true;
-
-  config.environment.systemPackages = with pkgs; [
-    htop
-    neovim
-  ];
 }
 
