b99031456fcef87216c60f0250e28579c4fbd422
Aiden Gindin <aiden@thegindins.com>
2023-12-11 13:52:48 -0500

    enable emacs for shadowfax

--- hosts/shadowfax/home.nix
+++ hosts/shadowfax/home.nix
@@ -6,6 +6,8 @@
 
   users.users.agindin.home = "/Users/agindin";
 
+  agindin.emacs.enable = true;
+
   home-manager.users.agindin = { pkgs, ... }: {
     home.packages = with pkgs; [
       thefuck
