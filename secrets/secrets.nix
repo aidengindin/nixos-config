44ad11871e032831d21d686fa805d596d08259c5
Aiden Gindin <aiden@thegindins.com>
2024-01-26 17:19:38 -0500

    add a secret to secrets.nix

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -3,6 +3,6 @@ let
   lorien = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINkD2Lt8RW1FK3HXQiNG20C9VdI1blj3Z/rdiSxfp63w aiden@aidengindin.com";
 in
 {
-  
+  "wallabag-db-password.age".publicKeys = [ lorien ];
 }
 
