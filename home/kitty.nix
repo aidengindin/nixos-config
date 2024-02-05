e8f702eefa357d04a1ecc893c9cbc8182e992f93
Aiden Gindin <aiden@thegindins.com>
2024-02-05 09:11:45 -0500

    move kitty config to kitty.conf

--- home/kitty.nix
+++ home/kitty.nix
@@ -8,22 +8,11 @@ in
     enable = mkEnableOption "kitty";
   };
 
-  config.home-manager.users.agindin.programs.kitty = mkIf cfg.enable {
-    enable = true;
-    theme = "Nord";
-    settings = {
-      cursor_blink_interval = 0;
-      enable_audio_bell = "no";
-      tab_bar_style = "powerline";
-
-      # confirm closing a window/tab only when a command is running
-      confirm_os_window_close = -1;
-
-      font_family = "Hasklug Nerd Font Mono";
-      bold_font = "Hasklug Nerd Font Mono Bold";
-      italic_font = "Hasklug Nerd Font Mono Italic";
-      bold_italic_font = "Hasklug Nerd Font Mono Bold Italic";
-      font_size = 12;
+  config.home-manager.users.agindin = mkIf cfg.enable {
+    programs.kitty = mkIf cfg.enable {
+      enable = true;
+      theme = "Nord";
     };
+    home.file."config/kitty/kitty.conf" = ./kitty/kitty.conf;
   };
 }
