60737831095d3e627521f5d30103bae1034023be
Aiden Gindin <aiden@aidengindin.com>
2024-02-21 20:12:32 -0500

    mount the sd card at the correct mount point

--- hosts/weathertop/sdCard.nix
+++ hosts/weathertop/sdCard.nix
@@ -26,7 +26,7 @@ in
 
   config = {
     services.udev.extraRules = ''
-      ACTION=="add", SUBSYSTEM=="block", KERNEL=="mmcblk[0-9]p[0-9]", ENV{ID_FS_USAGE}=="filesystem", RUN{program}+="${ pkgs.systemd }/bin/systemd-mount -o ${ lib.concatStringsSep "," cfg.btrfsOptions } --no-block --automount=yes --collect $devnode /run/media/mmcblk0p1"
+      ACTION=="add", SUBSYSTEM=="block", KERNEL=="mmcblk[0-9]p[0-9]", ENV{ID_FS_USAGE}=="filesystem", RUN{program}+="${ pkgs.systemd }/bin/systemd-mount -o ${ lib.concatStringsSep "," cfg.btrfsOptions } --no-block --automount=yes --collect $devnode /run/media/deck/mmcblk0p1"
     '';
   };
 }
