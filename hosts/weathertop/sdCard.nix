35fd674502c9db60bdf1ded7305e866310fb3bb7
Aiden Gindin <aiden@thegindins.com>
2024-02-22 09:56:50 -0500

    try using udiskie to get the sd card working

--- hosts/weathertop/sdCard.nix
+++ hosts/weathertop/sdCard.nix
@@ -1,32 +1,48 @@
-# stolen from https://github.com/appsforartists/device-config/blob/master/modules/sd-card.nix
+# stolen from https://github.com/seanybaggins/nix-home/blob/main/flake.nix
 
 { config, lib, pkgs, ... }:
 
-let
-  cfg = config.agindin.sdCard;
-in
 {
-  options.agindin.sdCard = {
-    btrfsOptions = lib.mkOption {
-      # from https://gitlab.com/popsulfr/steamos-btrfs/
-      default = [
-        "noatime"
-        "lazytime"
-        "compress-force=zstd"
-        "space_cache=v2"
-        "autodefrag"
-        "subvol=@"
-        "ssd_spread"
-      ];
-      description = ''
-        The mount options for a btrfs SD card.
-      '';
-    };
-  };
-
   config = {
-    services.udev.extraRules = ''
-      ACTION=="add", SUBSYSTEM=="block", KERNEL=="mmcblk[0-9]p[0-9]", ENV{ID_FS_USAGE}=="filesystem", RUN{program}+="${ pkgs.systemd }/bin/systemd-mount -o ${ lib.concatStringsSep "," cfg.btrfsOptions } --no-block --automount=yes --collect $devnode /run/media/deck/mmcblk0p1"
+    # Allow the SD card to be discovered from the gamespoce UI
+    users.groups.storage = { };
+    services.udisks2.enable = true;
+    services.udisks2.mountOnMedia = true;
+    systemd.services.udiskie = {
+      description = "Udiskie Automount Service";
+      wantedBy = [ "paths.target" ];
+      serviceConfig = {
+        ExecStart = "${pkgs.udiskie}/bin/udiskie --no-file-manager --no-notify";
+        Restart = "always";
+      };
+    };
+    
+    environment.etc."polkit-1/rules.d/50-udiskie.rules".text = ''
+      polkit.addRule(function(action, subject) {
+        var YES = polkit.Result.YES;
+        var permission = {
+          // required for udisks1:
+          "org.freedesktop.udisks.filesystem-mount": YES,
+          "org.freedesktop.udisks.luks-unlock": YES,
+          "org.freedesktop.udisks.drive-eject": YES,
+          "org.freedesktop.udisks.drive-detach": YES,
+          // required for udisks2:
+          "org.freedesktop.udisks2.filesystem-mount": YES,
+          "org.freedesktop.udisks2.encrypted-unlock": YES,
+          "org.freedesktop.udisks2.eject-media": YES,
+          "org.freedesktop.udisks2.power-off-drive": YES,
+          // required for udisks2 if using udiskie from another seat (e.g. systemd):
+          "org.freedesktop.udisks2.filesystem-mount-other-seat": YES,
+          "org.freedesktop.udisks2.filesystem-unmount-others": YES,
+          "org.freedesktop.udisks2.encrypted-unlock-other-seat": YES,
+          "org.freedesktop.udisks2.encrypted-unlock-system": YES,
+          "org.freedesktop.udisks2.eject-media-other-seat": YES,
+          "org.freedesktop.udisks2.power-off-drive-other-seat": YES
+        };
+        if (subject.isInGroup("storage")) {
+          return permission[action.id];
+        }
+      });
     '';
   };
 }
