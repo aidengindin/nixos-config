9bd22f311199501e95158c7af4a03c75d0f0c987
Aiden Gindin <aiden@aidengindin.com>
2024-03-19 20:30:23 -0400

    RIP yuzu

--- hosts/weathertop/system.nix
+++ hosts/weathertop/system.nix
@@ -35,10 +35,6 @@
   agindin.firefox.enable = true;
   agindin.gamingOptimizations.enable = true;
 
-  users.users.agindin.packages = with pkgs; [
-    yuzu-mainline
-  ];
-
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
