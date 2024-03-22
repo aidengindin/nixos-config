0b0ed79d36b446430c9dd83820e321cebcd29d0d
Aiden Gindin <aiden@thegindins.com>
2024-03-22 16:00:40 -0400

    add comments to flake.nix

--- flake.nix
+++ flake.nix
@@ -2,10 +2,14 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    # packages & configurations for steam deck
     jovian = {
       url = "github:Jovian-Experiments/Jovian-NixOS/";
       inputs.nixpkgs.follows = "unstable";
     };
+
+    # managing user environments - both stable & unstable modules
     home-manager = {
       url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -14,14 +18,20 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "unstable";
     };
+
+    # macos configurations
     darwin = {
       url = "github:lnl7/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    # managing docker containers
     arion = {
       url = "github:hercules-ci/arion";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    # secrets management
     agenix = {
       url = "github:ryantm/agenix";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -30,18 +40,25 @@
 
   outputs = { self, nixpkgs, unstable, jovian, home-manager, hm-unstable, darwin, arion, agenix }:
     let
+
+      # standard modules shared by all NixOS systems,
+      # with some conditional logic based on whether it tracks stable or unstable
       standardNixosModules = isUnstable: [
         (if isUnstable
           then hm-unstable.nixosModules.home-manager
           else home-manager.nixosModules.home-manager)
         agenix.nixosModules.default
       ];
+
+      # special args for all NixOS systems
       standardSpecialArgs = {
         inherit agenix;
       };
     in
     {
       nixosConfigurations = {
+
+        # a mini pc home server
         lorien = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
           specialArgs = standardSpecialArgs;
@@ -51,6 +68,7 @@
           ];
         };
 
+        # my steam deck
         weathertop = unstable.lib.nixosSystem {
           system = "x86_64-linux";
           specialArgs = standardSpecialArgs;
@@ -62,6 +80,8 @@
       };
 
       darwinConfigurations = {
+
+        # my main laptop
         shadowfax = darwin.lib.darwinSystem {
           system = "aarch64-darwin";
           modules = [
