796b28dc8360e2945081a079417fbeb207e554e0
Aiden Gindin <aiden@thegindins.com>
2024-08-11 10:39:22 -0400

    switch to 24.05 release and update flake.lock

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     # packages & configurations for steam deck
@@ -11,7 +11,7 @@
 
     # managing user environments - both stable & unstable modules
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.11";
+      url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     hm-unstable = {
