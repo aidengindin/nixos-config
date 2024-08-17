d8cab89630ae8b99d2e17c5c02c58481d1ffd0ee
Aiden Gindin <aiden@thegindins.com>
2024-08-17 16:33:00 -0400

    only read restic key if it exists

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -5,7 +5,9 @@ let
 
   shadowfaxUser = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICEOaGzXodczxTw7jpj/Tt1mQdkqnY5o9Ofh2ghHhOng aiden@thegindins.com";
   lorienUser = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINkD2Lt8RW1FK3HXQiNG20C9VdI1blj3Z/rdiSxfp63w aiden@aidengindin.com";
-  resticUser = builtins.readFile /var/lib/restic/.ssh/id_ed25519.pub;
+  resticUser = if builtins.pathExists /var/lib/restic/.ssh/id_ed25519.pub
+               then builtins.readFile /var/lib/restic/.ssh/id_ed25519.pub
+               else "";
 
   userKeys = [ shadowfaxUser lorienUser resticUser ];
 in
