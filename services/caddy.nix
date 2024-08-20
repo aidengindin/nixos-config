88f870bdeeae87ac1fd3611660a148dc38ba1853
Aiden Gindin <aiden@thegindins.com>
2024-08-20 17:27:30 -0400

    Revert "caddy: revert to acme staging"
    This reverts commit 7c37bbcd75090494a5fd8ce68f93a69b4ed80cb3.

--- services/caddy.nix
+++ services/caddy.nix
@@ -103,7 +103,6 @@ in
       email = "aiden+letsencrypt@aidengindin.com";
       globalConfig = ''
         acme_dns cloudflare {env.CLOUDFLARE_API_KEY}
-        acme_ca https://acme-staging-v02.api.letsencrypt.org/directory
       '';
       extraConfig = let
         tlsSetup = ''
