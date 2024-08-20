654ee0448a444a19ce062ca883ad151f96e5634f
Aiden Gindin <aiden@thegindins.com>
2024-08-20 15:22:50 -0400

    caddy: fix debug output

--- services/caddy.nix
+++ services/caddy.nix
@@ -152,10 +152,10 @@ in
           ];
           ExecStartPre = [
             "${pkgs.bash}/bin/bash -c 'echo CLOUDFLARE_API_KEY=$(cat $CREDENTIALS_DIRECTORY/cloudflare-api-key) > /tmp/caddy-env'"
-            "${pkgs.bash}/bin/bash -c 'echo \"CLOUDFLARE_API_KEY value: $CLOUDFLARE_API_KEY\" >> /tmp/caddy_debug.log'"
           ];
           environmentFile = "/tmp/caddy-env";
           ExecStartPost = [
+            "${pkgs.bash}/bin/bash -c 'echo \"CLOUDFLARE_API_KEY value: $CLOUDFLARE_API_KEY\" >> /tmp/caddy_debug.log'"  # TODO: remove
             "${pkgs.bash}/bin/bash -c 'rm -f /tmp/caddy-env'"
           ];
           AmbientCapabilities = "cap_net_bind_service";
