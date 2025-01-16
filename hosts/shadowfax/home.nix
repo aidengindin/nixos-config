02e70a7e0021ee2f05d67e223bfeb47de1565e82
Aiden Gindin <aiden@thegindins.com>
2025-01-16 16:54:26 -0500

    Add system path for Home Manager configuration

--- hosts/shadowfax/home.nix
+++ hosts/shadowfax/home.nix
@@ -8,6 +8,10 @@
 
   agindin.latex.enable = true;
 
+  environment.systemPath = [
+    "/opt/homebrew/bin"
+  ];
+
   # this line should not be edited even when upgrading NixOS versions
   home-manager.users.agindin.home.stateVersion = "23.11";
 }
