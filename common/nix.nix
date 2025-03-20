610b972e6e97bf74bd7f4dde1cb943f3009e862c
Aiden Gindin <aiden@thegindins.com>
2025-03-20 19:30:16 -0400

    configure nix optimization & garbage collection with platform-specific settings

--- common/nix.nix
+++ common/nix.nix
@@ -1,4 +1,9 @@
 { config, lib, pkgs, ... }:
+let
+  inherit (lib) mkIf strings;
+  isLinux = strings.hasInfix "linux" pkgs.system;
+  isDarwin = strings.hasInfix "darwin" pkgs.system;
+in
 {
   config = {
     nixpkgs.config.allowUnfree = true;
@@ -6,17 +11,27 @@
     nix = {
       package = pkgs.nixVersions.stable;
       extraOptions = ''
-          experimental-features = nix-command flakes
-        '';
-        optimise = {
-          automatic = true;
-          dates = [ "weekly" ];
+        experimental-features = nix-command flakes
+      '';
+      optimise = {
+        automatic = true;
+        dates = mkIf isLinux [ "weekly" ];
+        interval = mkIf isDarwin {
+          Weekday = 0;
+          Hour = 1;
+          Minute = 0;
         };
-        gc = {
-          automatic = true;
-          dates = "weekly";
-          options = "--delete-older-than 30d";
+      };
+      gc = {
+        automatic = true;
+        dates = mkIf isLinux "weekly";
+        interval = mkIf isDarwin {
+          Weekday = 0;
+          Hour = 0;
+          Minute = 0;
         };
+        options = "--delete-older-than 30d";
+      };
     };
   };
 }
