2f1d98eceb4a30680d15c6b5fe9fa599dde1961b
Aiden Gindin <aiden@thegindins.com>
2025-03-24 08:23:25 -0400

    make zellij optional & set theme

--- common/zellij.nix
+++ common/zellij.nix
@@ -1,9 +1,19 @@
 { config, pkgs, ... }:
+let
+  cfg = config.agindin.zellij;
+  inherit (lib) mkIf mkEnableOption;
 {
-  config = {
+  options.agindin.zellij = {
+    enable = mkEnableOption "zellij";
+  };
+
+  config = mkIf cfg.enable {
     home-manager.users.agindin.programs.zellij = {
       enable = true;
       enableBashIntegration = true;
+      settings = {
+        theme = "nord";
+      };
     };
   };
 }
--- hosts/shadowfax/home.nix
+++ hosts/shadowfax/home.nix
@@ -7,6 +7,7 @@
   agindin.latex.enable = true;
   agindin.kitty.enable = true;
   agindin.yabai.enable = true;
+  agindin.zellij.enable = true;
 
   # this line should not be edited even when upgrading NixOS versions
   home-manager.users.agindin.home.stateVersion = "23.11";
