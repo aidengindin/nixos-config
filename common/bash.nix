ae6f4ba7fe63091ac8cf768d8290e693780eda9a
Aiden Gindin <aiden@thegindins.com>
2025-04-01 14:00:59 -0400

    Enable vi mode in bash

--- common/bash.nix
+++ common/bash.nix
@@ -15,7 +15,23 @@
 
     programs.bash = {
       interactiveShellInit = lib.mkBefore ''
+        set -o vi
+
         source ${pkgs.blesh}/share/blesh/ble.sh
+
+        # Use more ergonomic keys for vi-mode navigation
+        ble-bind -m vi_nmap -f 'j' 'backward-char'
+        ble-bind -m vi_nmap -f 'l' '__atuin_history --shell-up-key-binding --keymap-mode=vim-normal'
+        ble-bind -m vi_nmap -f ';' 'forward-char'
+
+        # Change cursor shape based on vi mode
+        ble-bind -m vi_nmap --cursor 2
+        ble-bind -m vi_imap --cursor 6
+        ble-bind -m vi_omap --cursor 2
+        ble-bind -m vi_xmap --cursor 2
+        ble-bind -m vi_smap --cursor 4
+        ble-bind -m vi_cmap --cursor 2
+
         ble-import -f integration/zoxide
         eval "$(${pkgs.starship}/bin/starship init bash)"
         eval "$(${pkgs.atuin}/bin/atuin init bash)"
