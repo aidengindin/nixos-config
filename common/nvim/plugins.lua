f705f6c03e0d4438735eb19b3909e0666dc0fefe
Aiden Gindin <aiden@thegindins.com>
2025-04-03 07:21:41 -0400

    Add qmk plugin to neovim

--- common/nvim/plugins.lua
+++ common/nvim/plugins.lua
@@ -257,6 +257,22 @@ require("lazy").setup({
         }
       }
     end,
+  },
+  {
+    "codethread/qmk.nvim",
+    config = function ()
+      ---@type qmk.UserConfig
+      local conf = {
+        name = "LAYOUT_span_grid",
+        layout = {
+         "x x x x x _ x x x x x",
+         "x x x x x _ x x x x x",
+         "x x x x x _ x x x x x",
+         "_ _ x x x _ x x x _ _"
+       }
+     }
+     require("qmk").setup(conf)
+   end
   }
 })
 
