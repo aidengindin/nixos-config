8d5eea57541301cc121bc3981b6894ae52b87283
Aiden Gindin <aiden@thegindins.com>
2025-05-18 20:07:34 -0400

    use caddy's withPlugins feature

--- services/caddy.nix
+++ services/caddy.nix
@@ -18,20 +18,12 @@ let
   searxng = myServices.searxng;
 
   overlay = final: prev: {
-    caddy-cloudflare = prev.caddy.overrideAttrs (oldAttrs: rec {
-      vendorOverrides = {
-        "github.com/caddy-dns/cloudflare" = {
-          type   = "lockfile";
-          url    = "https://github.com/caddy-dns/cloudflare/archive/refs/tags/v0.2.1.tar.gz";
-          sha256 = "hH8fWCwldBxPOsvwXA7ZdgAnRr8F+yeue57q9tG8eHU=";
-        };
-      };
-
-      postBuild = ''
-        cd cmd/caddy
-        go build -trimpath -mod=vendor -o $out/bin/caddy
-      '';
-    });
+    caddy-cloudflare = prev.caddy.withPlugins {
+      plugins = [
+        "github.com/caddy-dns/cloudflare@v0.2.1"
+      ];
+      hash = "sha256-0000000000000000000000000000000000000000000000000000";
+    };
   };
 in
 {
@@ -48,86 +40,6 @@ in
   config = mkMerge [
     { nixpkgs.overlays = [ overlay ]; }
     (mkIf cfg.enable {
-
-      # we need to compile a custom build of caddy to include cloudflare dns support
-      # based on https://noah.masu.rs/posts/caddy-cloudflare-dns/
-      # nixpkgs.overlays = [
-        # (final: prev: {
-        #   caddy-cloudflare = prev.caddy.overrideAttrs (oldAttrs: rec {
-        #     vendorOverrides = {
-        #       "github.com/caddy-dns/cloudflare" = {
-        #         type = "lockfile";
-        #         url = "https://github.com/caddy-dns/cloudflare/archive/refs/tags/v0.2.1.tar.gz";
-        #         sha256 = "sha256-hH8fWCwldBxPOsvwXA7ZdgAnRr8F+yeue57q9tG8eHU=";
-        #       };
-        #     };
-  
-        #     postBuild = ''
-        #       cd cmd/caddy
-        #       go build -trimpath -mod=vendor -o $out/bin/caddy
-        #     '';
-        #   });
-        # })
-          # let
-          #   plugins = [ "github.com/caddy-dns/cloudflare" ];
-          #   goImports =
-          #     prev.lib.flip prev.lib.concatMapStrings plugins
-          #     (pkg: "   _ \"${pkg}\"\n");
-          #   goGets = prev.lib.flip prev.lib.concatMapStrings plugins
-          #     (pkg: "go get ${pkg}\n      ");
-          #   main = ''
-          #     package main
-          #     import (
-          #     	caddycmd "github.com/caddyserver/caddy/v2/cmd"
-          #     	_ "github.com/caddyserver/caddy/v2/modules/standard"
-          #     ${goImports}
-          #     )
-          #     func main() {
-          #     	caddycmd.Main()
-          #     }
-          #   '';
-  
-          # in {
-          #   caddy-cloudflare = prev.buildGoModule {
-          #     pname = "caddy-cloudflare";
-          #     version = prev.caddy.version;
-          #     runVend = true;
-  
-          #     subPackages = [ "cmd/caddy" ];
-  
-          #     src = prev.caddy.src;
-  
-          #     vendorHash = "sha256-u5vqVsP3Vg/4hHWyWdSkkYoLMJoRTdVYVj2994lcAeQ=";
-  
-          #     overrideModAttrs = (_: {
-          #       preBuild = ''
-          #         echo '${main}' > cmd/caddy/main.go
-          #         ${goGets}
-          #       '';
-          #       postInstall = "cp go.sum go.mod $out/ && ls $out/";
-          #     });
-  
-          #     postPatch = ''
-          #       echo '${main}' > cmd/caddy/main.go
-          #       cat cmd/caddy/main.go
-          #     '';
-  
-          #     postConfigure = ''
-          #       cp vendor/go.sum ./
-          #       cp vendor/go.mod ./
-          #     '';
-  
-          #     meta = with prev.lib; {
-          #       mainProgram = "caddy";
-          #       homepage = "https://caddyserver.com";
-          #       description =
-          #         "Fast, cross-platform HTTP/2 web server with automatic HTTPS";
-          #       license = licenses.asl20;
-          #       maintainers = with maintainers; [ Br1ght0ne techknowlogick ];
-          #     };
-          #   };
-          # }
-      # ];
   
       users.users.caddy = {
         isSystemUser = true;
