4d88d25e46c87cb2dd2d6702011fc975aebfc0a6
Aiden Gindin <aiden@thegindins.com>
2025-05-26 09:49:46 -0400

    Clean up neovim plugins

--- common/nvim/plugins.lua
+++ common/nvim/plugins.lua
@@ -12,9 +12,8 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
+  -- Appearance
   "shaunsingh/nord.nvim",
-  "tpope/vim-surround",
-  "folke/which-key.nvim",
   {
     "nvim-lualine/lualine.nvim",
     config = function()
@@ -23,16 +22,6 @@ require("lazy").setup({
       }
     end,
   },
-  {
-    "NeogitOrg/neogit",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "sindrets/diffview.nvim",
-      "ibhagwan/fzf-lua",
-    },
-    config = true
-  },
-  "mbbill/undotree",
   {
     "akinsho/bufferline.nvim",
     version = "*",
@@ -43,6 +32,71 @@ require("lazy").setup({
     main = "ibl",
     opts = {}
   },
+
+  -- Core functionality
+  "tpope/vim-surround",
+  "folke/which-key.nvim",
+  "mbbill/undotree",
+  {
+    "m4xshen/autoclose.nvim",
+    config = function ()
+      require("autoclose").setup({
+        options = {
+          pair_spaces = true,
+        }
+      })
+    end
+  },
+
+  -- File management and navigation
+  {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    opts = {
+      bigfile = { enabled = true },
+      dashboard = { enabled = true },
+      explorer = { enabled = true },
+      indent = { enabled = true },
+      input = { enabled = true },
+      picker = { enabled = true },
+      notifier = { enabled = true },
+      quickfile = { enabled = true },
+      scope = { enabled = true },
+      scroll = { enabled = true },
+      statuscolumn = { enabled = true },
+      words = { enabled = true }
+    }
+  },
+  {
+    "mikavilpas/yazi.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "folke/snacks.nvim"
+    }
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons"
+    }
+  },
+
+  -- Git integration
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "sindrets/diffview.nvim",
+      "ibhagwan/fzf-lua",
+    },
+    config = true
+  },
+  "airblade/vim-gitgutter",
+
+  -- Syntax and language support
   {
     "nvim-treesitter/nvim-treesitter",
     config = function()
@@ -50,8 +104,7 @@ require("lazy").setup({
         highlight = {
           enable = true;
         },
-        ensure_installed = {  -- just enabling everything I use
-          -- "awk",  -- not supported yet
+        ensure_installed = {
           "bash",
           "bibtex",
           "c",
@@ -99,15 +152,8 @@ require("lazy").setup({
     "vim-pandoc/vim-pandoc",
     dependencies = { "vim-pandoc/vim-pandoc-syntax" }
   },
-  "airblade/vim-gitgutter",
-  {
-    "nvim-telescope/telescope.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-treesitter/nvim-treesitter",
-      "nvim-tree/nvim-web-devicons"
-    }
-  },
+
+  -- LSP and completion
   {
     "williamboman/mason.nvim",
     config = function()
@@ -148,83 +194,6 @@ require("lazy").setup({
       lspconfig.rust_analyzer.setup {}
     end
   },
-  -- {
-  --   "yetone/avante.nvim",
-  --   event = "VeryLazy",
-  --   version = false,
-  --   opts = {
-  --     provider = "claude",
-  --     claude = {
-  --       endpoint = "https://api.anthropic.com",
-  --       model = "claude-sonnet-4-0",
-  --       timeout = 30000,
-  --       temperature = 0,
-  --       max_tokens = 4096,
-  --       disable_tools = false
-  --     }
-  --   },
-  --   build = "make",
-  --   dependencies = {
-  --     "nvim-treesitter/nvim-treesitter",
-  --     "stevearc/dressing.nvim",
-  --     "MunifTanjim/nui.nvim",
-  --     "echasnovski/mini.pick",
-  --     "nvim-telescope/telescope.nvim",
-  --     "hrsh7th/nvim-cmp",
-  --     "ibhagwan/fzf-lua",
-  --     {
-  --       "HakonHarnes/img-clip.nvim",
-  --       event = "VeryLazy",
-  --       opts = {
-  --         default = {
-  --           embed_image_as_base64 = false,
-  --           prompt_for_file_name = false,
-  --           drag_and_drop = {
-  --             insert_mode = true,
-  --           },
-  --           use_absolute_path = true,
-  --         },
-  --       },
-  --     },
-  --     {
-  --       'MeanderingProgrammer/render-markdown.nvim',
-  --       opts = {
-  --         file_types = { "markdown", "Avante" },
-  --       },
-  --       ft = { "markdown", "Avante" },
-  --     }
-  --   }
-  -- },
-  -- {
-  --   "milanglacier/minuet-ai.nvim",
-  --   dependencies = {
-  --     "nvim-lua/plenary.nvim",
-  --   },
-  --   config = function()
-  --     require("minuet").setup {
-  --       provider = "claude",
-  --       claude = {
-  --         api_key = vim.env.ANTHROPIC_API_KEY,
-  --         model = "claude-sonnet-4-0",
-  --         temperature = 0.2,
-  --         max_tokens = 1024,
-  --       },
-  --       blink = {
-  --         enabled = true,
-  --         score_offset = 8,
-  --       },
-  --     }
-  --   end
-  -- },
-  {
-    "folke/lazydev.nvim",
-    ft = "lua",
-    opts = {
-      library = {
-        "~/nixos-config/common/nvim"
-      }
-    }
-  },
   {
     "Saghen/blink.cmp",
     dependencies = {
@@ -239,17 +208,9 @@ require("lazy").setup({
             force_version = "v1.0.0"
           }
         },
-        -- keymap = {
-        --   ["<A-y>"] = require("minuet").make_blink_map()
-        -- },
         sources = {
           default = { "lazydev", "lsp", "path", "buffer", "snippets" },
           providers = {
-            -- minuet = {
-            --   name = "minuet",
-            --   module = "minuet.blink",
-            --   score_offset = 8
-            -- },
             lazydev = {
               name = "LazyDev",
               module = "lazydev.integrations.blink",
@@ -265,59 +226,8 @@ require("lazy").setup({
       }
     end,
   },
-  {
-    "codethread/qmk.nvim",
-    config = function ()
-      ---@type qmk.UserConfig
-      local conf = {
-        name = "LAYOUT_split_3x5_3",
-        layout = {
-         "x x x x x _ x x x x x",
-         "x x x x x _ x x x x x",
-         "x x x x x _ x x x x x",
-         "_ _ x x x _ x x x _ _"
-       }
-     }
-     require("qmk").setup(conf)
-   end
-  },
-  {
-    "folke/snacks.nvim",
-    priority = 1000,
-    lazy = false,
-    ---@type snacks.Config
-    opts = {
-      bigfile = { enabled = true },
-      dashboard = { enabled = true },
-      explorer = { enabled = true },
-      indent = { enabled = true },
-      input = { enabled = true },
-      picker = { enabled = true },
-      notifier = { enabled = true },
-      quickfile = { enabled = true },
-      scope = { enabled = true },
-      scroll = { enabled = true },
-      statuscolumn = { enabled = true },
-      words = { enabled = true }
-    }
-  },
-  {
-    "mikavilpas/yazi.nvim",
-    event = "VeryLazy",
-    dependencies = {
-      "folke/snacks.nvim"
-    }
-  },
-  {
-    "m4xshen/autoclose.nvim",
-    config = function ()
-      require("autoclose").setup({
-        options = {
-          pair_spaces = true,
-        }
-      })
-    end
-  },
+
+  -- AI assistance
   {
     "greggh/claude-code.nvim",
     dependencies = {
@@ -329,6 +239,32 @@ require("lazy").setup({
   },
   {
     "github/copilot.vim"
+  },
+
+  -- Development tools
+  {
+    "folke/lazydev.nvim",
+    ft = "lua",
+    opts = {
+      library = {
+        "~/nixos-config/common/nvim"
+      }
+    }
+  },
+  {
+    "codethread/qmk.nvim",
+    config = function ()
+      local conf = {
+        name = "LAYOUT_split_3x5_3",
+        layout = {
+         "x x x x x _ x x x x x",
+         "x x x x x _ x x x x x",
+         "x x x x x _ x x x x x",
+         "_ _ x x x _ x x x _ _"
+       }
+     }
+     require("qmk").setup(conf)
+   end
   }
 })
 
@@ -340,4 +276,3 @@ vim.diagnostic.config({
   signs = true,
   update_in_insert = false
 })
-
