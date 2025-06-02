d373927e084fac56f48ca6eb91754a91aa39c3c8
Aiden Gindin <aiden@thegindins.com>
2025-06-02 17:09:00 -0400

    Ensure nav key remappings apply to all buffers

--- common/nvim/init.lua
+++ common/nvim/init.lua
@@ -5,26 +5,32 @@ vim.wo.relativenumber = true
 -- enable mouse for all modes
 o.mouse = 'a'
 
--- Remap navigation keys to jkl; instead of hjkl
-vim.keymap.set('n', 'j', 'h', { noremap = true, silent = true })
-vim.keymap.set('n', 'k', 'gj', { noremap = true, silent = true })
-vim.keymap.set('n', 'l', 'gk', { noremap = true, silent = true })
-vim.keymap.set('n', ';', 'l', { noremap = true, silent = true })
+-- Define a function to apply our navigation remappings
+local function apply_navigation_keymaps(opts)
+  opts = opts or { noremap = true, silent = true }
 
--- Replace ; with h in normal mode to compensate
-vim.keymap.set('n', 'h', ';', { noremap = true, silent = true })
+  -- Normal mode
+  vim.keymap.set('n', 'j', 'h', opts)
+  vim.keymap.set('n', 'k', 'gj', opts)
+  vim.keymap.set('n', 'l', 'gk', opts)
+  vim.keymap.set('n', ';', 'l', opts)
+  vim.keymap.set('n', 'h', ';', opts)
 
--- Apply same remappings to visual mode for consistency
-vim.keymap.set('v', 'j', 'h', { noremap = true, silent = true })
-vim.keymap.set('v', 'k', 'gj', { noremap = true, silent = true })
-vim.keymap.set('v', 'l', 'gk', { noremap = true, silent = true })
-vim.keymap.set('v', ';', 'l', { noremap = true, silent = true })
+  -- Visual mode
+  vim.keymap.set('v', 'j', 'h', opts)
+  vim.keymap.set('v', 'k', 'gj', opts)
+  vim.keymap.set('v', 'l', 'gk', opts)
+  vim.keymap.set('v', ';', 'l', opts)
 
--- Apply to operator-pending mode for commands like d, y, c
-vim.keymap.set('o', 'j', 'h', { noremap = true, silent = true })
-vim.keymap.set('o', 'k', 'gj', { noremap = true, silent = true })
-vim.keymap.set('o', 'l', 'k', { noremap = true, silent = true })
-vim.keymap.set('o', ';', 'l', { noremap = true, silent = true })
+  -- Operator-pending mode
+  vim.keymap.set('o', 'j', 'h', opts)
+  vim.keymap.set('o', 'k', 'gj', opts)
+  vim.keymap.set('o', 'l', 'k', opts)
+  vim.keymap.set('o', ';', 'l', opts)
+end
+
+-- Apply keymaps globally
+apply_navigation_keymaps()
 
 -- search-related options
 o.ignorecase = true  -- case insensitive search...
@@ -49,4 +55,40 @@ vim.diagnostic.config({
 
 require("plugins")
 require("keybindings")
+
+-- Create autocommands to apply keymaps for specific filetypes
+vim.api.nvim_create_autocmd({"FileType"}, {
+  pattern = {"markdown", "pandoc"},
+  callback = function()
+    apply_navigation_keymaps({ noremap = true, silent = true, buffer = true })
+  end,
+})
+
+-- Create autocommand group for Neogit with high priority
+local neogit_group = vim.api.nvim_create_augroup("NeogitKeymaps", { clear = true })
+
+-- Create autocommand for Neogit buffers
+vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
+  pattern = {"*"},
+  group = neogit_group,
+  callback = function()
+    local bufname = vim.fn.bufname('%')
+    if bufname:match("^Neogit") then
+      vim.defer_fn(function()
+        -- Apply our navigation keymaps
+        apply_navigation_keymaps({ noremap = true, silent = true, buffer = true })
+
+        -- Add back Neogit's log functionality on 'j' key
+        vim.keymap.set('n', 'j', function()
+          if bufname:match("^Neogit: Status") then
+            require("neogit.actions").popup.log()
+          else
+            -- Default to our remapped 'h' behavior in other Neogit buffers
+            vim.cmd("normal! h")
+          end
+        end, { noremap = true, silent = true, buffer = true })
+      end, 100)
+    end
+  end
+})
 
