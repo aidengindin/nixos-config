f37df6d5505a18c03e635478cf28f1608b51209a
Aiden Gindin <aiden@thegindins.com>
2025-06-18 13:08:17 -0400

    Add catppuccin theming for mpv

--- common/mpv.nix
+++ common/mpv.nix
@@ -20,7 +20,25 @@ in
         "k" = "add volume -2";
         "l" = "add volume 2";
       };
+      config = {
+        background-color = "#1e1e2e";
+        osd-back-color = "#11111b";
+        osd-border-color = "#11111b";
+        osd-color = "#cdd6f4";
+        osd-shadow-color = "#1e1e2e";
+      };
+      scriptOpts = {
+        stats = {
+          border_color = "251818";
+          font_color = "f4d6cd";
+          plot_bg_border_color = "fab489";
+          plot_bg_color = "251818";
+          plot_color = "fab489";
+        };
+        uosc = {
+          color = "foreground=89b4fa,foreground_text=313244,background=1e1e2e,background_text=cdd6f4,curtain=181825,success=a6e3a1,error=f38ba8";
+        };
+      };
     };
   };
 }
-
