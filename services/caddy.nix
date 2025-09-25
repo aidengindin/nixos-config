0ed0fc4d5fcab0a872bf74cc5ae9137d946e583a
Aiden Gindin <aiden@aidengindin.com>
2025-09-25 08:26:48 -0400

    update caddy hash

--- services/caddy.nix
+++ services/caddy.nix
@@ -24,7 +24,7 @@ let
       plugins = [
         "github.com/caddy-dns/cloudflare@v0.2.1"
       ];
-      hash = "sha256-AcWko5513hO8I0lvbCLqVbM1eWegAhoM0J0qXoWL/vI=";
+      hash = "sha256-j+xUy8OAjEo+bdMOkQ1kVqDnEkzKGTBIbMDVL7YDwDY=";
     };
   };
 in
