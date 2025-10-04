38191625884893f17408d34eb3e850c73cd1335d
Aiden Gindin <aiden@aidengindin.com>
2025-10-04 16:04:29 -0400

    add shortcut to adjust keyboard brightness

--- linux/hypr/scripts/brightness.sh
+++ linux/hypr/scripts/brightness.sh
@@ -2,28 +2,34 @@
 
 # Get current brightness percentage
 get_brightness() {
-    brightnessctl -m | cut -d',' -f4 | tr -d '%'
+  brightnessctl -m | cut -d',' -f4 | tr -d '%'
 }
 
 # Send notification
 notify_brightness() {
-    brightness=$(get_brightness)
-    notify-send -h string:x-canonical-private-synchronous:sys-notify -u low "Brightness" "${brightness}%" \
-        -h int:value:$brightness -t 1500
+  brightness=$(get_brightness)
+  notify-send -h string:x-canonical-private-synchronous:sys-notify -u low "Brightness" "${brightness}%" \
+    -h int:value:$brightness -t 1500
 }
 
 # Main
 case "$1" in
-    up)
-        brightnessctl set +5%
-        notify_brightness
-        ;;
-    down)
-        brightnessctl set 5%-
-        notify_brightness
-        ;;
-    *)
-        echo "Usage: $0 {up|down}"
-        exit 1
-        ;;
+  up)
+    brightnessctl set +5%
+    notify_brightness
+    ;;
+  down)
+    brightnessctl set 5%-
+    notify_brightness
+    ;;
+  keyup)
+    brightnessctl -d 'chromeos::kbd_backlight' set +10%
+    ;;
+  keydown)
+    brightnessctl -d 'chromeos::kbd_backlight' set 10%-
+    ;;
+  *)
+    echo "Usage: $0 {up|down}"
+    exit 1
+    ;;
 esac
