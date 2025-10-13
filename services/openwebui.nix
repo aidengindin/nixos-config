ce8c6326adb1e03b2cf5a9331cd5dc71a0b58092
Aiden Gindin <aiden@aidengindin.com>
2025-10-12 20:32:12 -0400

    update flake inputs & image tags

--- services/openwebui.nix
+++ services/openwebui.nix
@@ -9,7 +9,7 @@ in
     tag = mkOption {
       # https://github.com/open-webui/open-webui/releases
       type = types.str;
-      default = "git-598282c";
+      default = "git-8d7d79d";
       description = "Tag of the openwebui image to use";
     };
     subnet = mkOption {
