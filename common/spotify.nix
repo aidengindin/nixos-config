ec264a1dd32a857cbaa1a4af2b23ec0d91d8f8b6
Aiden Gindin <aiden@aidengindin.com>
2025-11-23 16:55:05 -0500

    use unstable version of spotify-player

--- common/spotify.nix
+++ common/spotify.nix
@@ -2,21 +2,6 @@
 let
   cfg = config.agindin.spotify;
   inherit (lib) mkIf mkEnableOption;
-  
-  # spotify-player-main = unstablePkgs.spotify-player.overrideAttrs (oldAttrs: rec {
-  #   version = "main";
-  #   src = pkgs.fetchFromGitHub {
-  #     owner = "aome510";
-  #     repo = "spotify-player";
-  #     rev = "master";
-  #     hash = "sha256-yjm5NFW+6vEyv45AVfwx+6w2dJ3lKj/UM2NQhGW5SSs=";
-  #   };
-  #   cargoDeps = pkgs.rustPlatform.fetchCargoVendor {
-  #     inherit src;
-  #     name = "spotify-player-main";
-  #     hash = "sha256-rqDLkzCl7gn3s/37MPytYaGb0tdtemYi8bgEkrkllDU=";
-  #   };
-  # });
 in
 {
   options.agindin.spotify = {
@@ -25,7 +10,7 @@ in
   config = mkIf cfg.enable {
     home-manager.users.agindin.programs.spotify-player = {
       enable = true;
-      # package = spotify-player-main;
+      package = unstablePkgs.spotify-player;
     };
   };
 }
