02309706564cb12d2eab3ecbd41f7a26d3936e6d
Aiden Gindin <aiden@aidengindin.com>
2026-01-24 16:06:52 -0500

    fix(networking): disable wifi when a wired connected is enabled
    khazad-dum was experiencing an arp flux issue where the wifi network
    interface would be used when the wired interface should have been used.
    this led to ipv4 connections being nonfunctional. to fix this, a simple
    script disables wifi when a wired connection is established.

--- linux/network.nix
+++ linux/network.nix
@@ -1,9 +1,36 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 {
   config = {
     networking.networkmanager.enable = true;
     services.tailscale.enable = true;
     systemd.services.NetworkManager-wait-online.enable = lib.mkForce false;
+
+    # specific to khazad-dum / laptops: disable wifi when wired to prevent arp flux
+    networking.networkmanager.dispatcherScripts = [
+      {
+        source = pkgs.writeText "wifi-wired-exclusive" ''
+          interface=$1
+          status=$2
+
+          if [[ "$interface" =~ ^en.* ]]; then
+            case $status in
+              up)
+                ${pkgs.networkmanager}/bin/nmcli radio wifi off
+                ;;
+              down)
+                ${pkgs.networkmanager}/bin/nmcli radio wifi on
+                ;;
+            esac
+          fi
+        '';
+        type = "basic";
+      }
+    ];
 
     agindin.impermanence.systemDirectories = lib.mkIf config.agindin.impermanence.enable [
       "/var/lib/tailscale"
@@ -11,4 +38,3 @@
     ];
   };
 }
-
