28f847d71f0f5231e2f27326c8479695c935c9c1
Aiden Gindin <aiden@aidengindin.com>
2026-05-15 18:29:21 -0400

    chore(nix): add binary caches for hyprland & jovian

--- linux/nix.nix
+++ linux/nix.nix
@@ -2,6 +2,20 @@
 {
   config = {
     nix = {
+      settings = {
+        substituters = [
+          "https://hyprland.cachix.org"
+          "https://jovian.cachix.org"
+        ];
+        trusted-substituters = [
+          "https://hyprland.cachix.org"
+          "https://jovian.cachix.org"
+        ];
+        trusted-public-keys = [
+          "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+          "jovian.cachix.org-1:HTTwRiPJkXkOBY39jGZBsZ1uXWvK3I1wYm/04q9+pP8="
+        ];
+      };
       extraOptions = ''
         experimental-features = nix-command flakes
       '';
